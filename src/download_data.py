39cad4ded16e6e8481571148f9f99f97fd19b34b
Fatih Tuna <36865654+tunafatih@users.noreply.github.com>
2021-11-13 14:57:05 +0300

    download_data.py added
    Downloads the file from s3

---
+++ src/download_data.py
@@ -0,0 +1,5 @@
+import boto3
+
+client = boto3.client('s3', aws_access_key_id = 'AKIAR6FH7HG7PJFCEFJV', aws_secret_access_key = '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')
+
+client.download_file('fatihtuna-case', 'e-commerce.zip', '/Users/fatihtuna/Desktop/volt_lines/e-commerce.zip')
