0373f8424552480a15fa0259dbc8de373819a881
Fatih Tuna <36865654+tunafatih@users.noreply.github.com>
2021-11-17 22:09:58 +0300

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,5 @@
 import boto3
 
-s3 = boto3.client('s3', aws_access_key_id = 'AKIAR6FH7HG7PJFCEFJV', aws_secret_access_key = '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')
+s3 = boto3.client('s3', aws_access_key_id = ' ', aws_secret_access_key = ' ')
 
 s3.download_file('fatihtuna-case', 'e-commerce.zip', 'brazilian-ecommerce/e-commerce.zip')
